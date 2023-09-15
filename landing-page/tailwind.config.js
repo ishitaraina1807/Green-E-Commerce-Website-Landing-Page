d9070611b27d7729da035db9e89d41d3d980a3cf
ishitaraina1807 <ishitaraina18@gmail.com>
2023-09-15 15:56:15 +0530

    added components

--- landing-page/tailwind.config.js
+++ landing-page/tailwind.config.js
@@ -6,6 +6,19 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+      'custom-black': '#000000',
+      'custom-white': '#FFFFFF',
+      'dark-green': '#228F47',
+      'light-green': '#66BF84',
+      'very-light-grey': '#D9D9D9',
+      'custom-yellow': '#EBC351',
+      'very-light-green': '#E7FFEF',
+      'custom-white-2': '#FCFCFC',
+      'very-dark-green': '#0E4A22',
+      'light-green-2': '#5AAA75',
+      'dark-grey' : '#6C6C6C',
+      'custom-white-3' : '#F9F9F9',
+      'light-grey': '#A1A1A1'
       },
     },
   },
